5b8944b9ce788ff95b9fea8bfee0c2a2b384b886
raratii <purnamaratih419@gmail.com>
2024-11-01 08:15:15 +0700

    commitpertanyyan1

--- ArrayBilangan20.java
+++ ArrayBilangan20.java
@@ -14,11 +14,8 @@ public class ArrayBilangan20 {
         bil[2] = -7;
         bil[3] = 17;
 
-        System.out.println(bil[0]);
-        System.out.println(bil[1]);
-        System.out.println(bil[2]);
-        System.out.println(bil[3]);
-
-        
+        for (int i = 0; i < 4; i++) {
+            System.out.println(bil[i]);
+        }   
     }
 }
