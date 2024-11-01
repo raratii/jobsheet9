815b3663f9d7c7e33cc98b0be2d8cf3fb18432ce
raratii <purnamaratih419@gmail.com>
2024-11-01 09:02:58 +0700

    commitpertanyaan3

--- ArrayRataNilai20.java
+++ ArrayRataNilai20.java
@@ -9,18 +9,33 @@ public class ArrayRataNilai20 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[] nilaiMhs = new int[10];
-        double total = 0;
-        double rata2;
+        double totalLulus= 0,totalTidakLulus = 0;
+        int lulus=0, tidakLulus=0;
+        
+        System.out.print("Masukkan jumlah mahasiswa : ");
+        int jumlahMhs = sc.nextInt();
+
+        int[] nilaiMhs = new int[8];
 
         for (int i = 0; i < nilaiMhs.length; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" +(i+1)+" : ");
             nilaiMhs[i] = sc.nextInt();
+
+            if (nilaiMhs[i]>70) {
+                totalLulus += nilaiMhs[i];
+                lulus++;
+            } else {
+                totalTidakLulus += nilaiMhs[i];
+                tidakLulus++;
+            }
         }
-        for (int i= 0; i <nilaiMhs.length; i++ ) {
-            total += nilaiMhs[i];
-        }
-        rata2 = total/nilaiMhs.length;
-        System.out.println("Rata-rata nilai =" +rata2);
+        
+        double rataLulus = (lulus > 0) ? totalLulus/lulus : 0;
+        double rataTidakLulus = (tidakLulus > 0) ? totalTidakLulus/tidakLulus : 0 ;
+    
+        System.out.println("Rata-rata nilai lulus =" +rataLulus);
+        System.out.println("Rata-rata nilai tidak lulus =" +rataTidakLulus);
     }
 }
+
+    
